4bda8ee410a2824466551ceefc302e4eb7afcfa8
Cameron Chan <cameron.chan@advancednavigation.com>
2023-11-13 13:50:42 +1100

    add init py

---
+++ src/cgcpluginlib/__init__.py
@@ -0,0 +1,6 @@
+from .jsonobject import JsonObject
+from .general import LabelType, ColourIndexType, Marker, MarkerType
+from .istream import IStream, IStreamType 
+from .geo import GeoLocation, GeoLocationBase, Angular, GeoInfo, GeoPolygon, GeoInfoType 
+from .ostream import OStream, OStreamType 
+from .visualobject import VoClickMap, VoPoint, VoVector, VoBox, VoImage, VoVertex, VoPolygon, VisualObjectType
